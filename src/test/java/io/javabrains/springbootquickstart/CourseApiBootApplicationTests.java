d74c16e3e812c00b67566fd036290d484b4b55de
Oleksiy Blavat <oleksiy.blavat@verizonmedia.com>
2020-01-18 01:18:49 -0500

    Freshly generated project through start.spring.io

---
+++ src/test/java/io/javabrains/springbootquickstart/CourseApiBootApplicationTests.java
@@ -0,0 +1,13 @@
+package io.javabrains.springbootquickstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApiBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
